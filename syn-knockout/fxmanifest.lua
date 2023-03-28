535c2102ef167b019e135074b8940d8d8396b53a
ꜱʏɴ <68836355+syn1xx@users.noreply.github.com>
2023-03-28 01:02:52 -0400

    Add files via upload

---
+++ syn-knockout/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Syn'
+description 'discord.gg/fivemesx'
+
+client_scripts {
+    'knockout.lua',
+    'config.lua'
+}
+
+-- credits: https://github.com/zharrane/Knock-out-Script
